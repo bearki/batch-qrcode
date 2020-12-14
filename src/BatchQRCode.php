d1ee1e2f28f0a2fb93b6c0d19d82e0b4a754c524
yourxkc <508023077@qq.com>
2020-12-15 02:04:54 +0800

    批量Excel生成二维码开发完成

--- src/BatchQRCode.php
+++ src/BatchQRCode.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Excel批量生成QR二维码
- * @version 1.0.0
+ * @version 1.0.0 2020-12-15 02:00
  * @author Bearke <508023077@qq.com>
  * @package Bearke
  * @link https://github.com/phpoffice/phpspreadsheet
@@ -11,31 +11,51 @@
 namespace Bearke;
 
 use Exception;
+use Endroid\QrCode\QrCode;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\LabelAlignment;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class BatchQRCode
 {
     // 要导入的Excel文件路径
     private $excelPath = '';
+
     // 二维码内容前缀
     private $qrcodePrefix = '';
     // 生成二维码的Excel表头
     private $qrcodeArr = [];
     // 二维码内容后缀
     private $qrcodeSuffix = '';
-    // 二维码内容前缀
+
+    // 二维码描述内容前缀
     private $describePrefix = '';
     // 描述二维码的Excel表头
     private $describeArr = [];
-    // 二维码内容前缀
+    // 二维码描述内容后缀
     private $describeSuffix = '';
-    // 图片整体宽度
-    private $imgWidth = 40;
-    // 图片整体高度
-    private $imgHeight = 40;
+
+    // 二维码宽高（宽==高）
+    private $imgWidthHeight = 300;
+    // 二维码外边距
+    private $imgMargin = 10;
     // 字体大小
     private $fontSize = 14;
+
+    // 二维码颜色
+    private $qrcodeColor = ['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0];
+    // 二维码背景色
+    private $qrcodeBackground = ['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0];
+
+    // 二维码LOGO文件路径
+    private $logoPath = '';
+    // 二维码LOGO宽度
+    private $logoWidth = 50;
+    // 二维码LGO高度
+    private $logoHeight = 50;
+
     // 二维码保存路径
-    private $savePath = './qrcode/img/';
+    private $savePath = './static/qrcode/img/';
 
     /**
      * 构造函数
@@ -45,7 +65,7 @@ class BatchQRCode
     public function __construct(string $excelPath = '')
     {
       if (!empty($excelPath)) {
-        $this->indExcelPath($excelPath);
+        $this->setExcelPath($excelPath);
       }
     }
 
@@ -53,9 +73,9 @@ class BatchQRCode
      * 设置Excel文件所在路径
      *
      * @param string $excelPath excel文件所在路径
-     * @return bool 文件是否引入成功
+     * @return object
      */
-    public function indExcelPath(string $excelPath)
+    public function setExcelPath(string $excelPath)
     {
         // 截取最后一个字符，判断是x还是s
         $suffix_x_s = strtolower(substr($excelPath, -1, 1)); // 转换字符为小写
@@ -81,21 +101,23 @@ class BatchQRCode
         if (file_exists($excelPath)) {
           // 设置Excel文件路径
           $this->excelPath = $excelPath;
-          return true;
+          return $this;
+        } else {
+            // 抛出文件不存在
+            throw new Exception('Error: file does not exist');
         }
-        return false;
     }
 
     /**
      * 设置指定范围字段为二维码内容
      *
      * @param array $fieldList 支持多字段，顺序影响拼接
-     * @return void
+     * @return object
      */
-    public function selectFieldCodeContent(array $fieldList)
+    public function setQRCodeField(array $fieldList)
     {
         $this->qrcodeArr = $fieldList;
-        return;
+        return $this;
     }
 
     /**
@@ -103,25 +125,25 @@ class BatchQRCode
      *
      * @param string $prefix 二维码内容前缀
      * @param string $suffix 二维码内容后缀
-     * @return void
+     * @return object
      */
-    public function setContentPrefixAndSuffix(string $prefix, string $suffix)
+    public function setQRCodePrefixSuffix(string $prefix, string $suffix)
     {
         $this->qrcodePrefix = $prefix;
         $this->qrcodeSuffix = $suffix;
-        return;
+        return $this;
     }
 
     /**
      * 设置指定范围字段为二维码下方描述内容
      *
      * @param array $fieldList 支持多字段，顺序影响拼接
-     * @return void
+     * @return object
      */
-    public function selectFieldCodeDescribe(array $fieldList)
+    public function setDescrField(array $fieldList)
     {
         $this->describeArr = $fieldList;
-        return;
+        return $this;
     }
 
     /**
@@ -129,22 +151,22 @@ class BatchQRCode
      *
      * @param string $prefix
      * @param string $suffix
-     * @return void
+     * @return object
      */
-    public function setDescribePrefixAndSuffix(string $prefix, string $suffix)
+    public function setDescrPrefixSuffix(string $prefix, string $suffix)
     {
         $this->describePrefix = $prefix;
         $this->describeSuffix = $suffix;
-        return;
+        return $this;
     }
 
     /**
-     * 设置二维码图片保存的文件夹
+     * 设置二维码图片保存的路径
      *
      * @param string $path 仅支持文件夹
-     * @return void
+     * @return object
      */
-    public function setQRCodeImgSavePath(string $path)
+    public function setSavePath(string $path)
     {
         // 判断目录尾部是否有斜杠
         if (substr($path, -1, 1) === "/") {
@@ -164,20 +186,62 @@ class BatchQRCode
         }
         // 配置二维码图片保存路径
         $this->savePath = $path . '/';
-        return;
+        return $this;
     }
 
     /**
-     * 设置二维码图片宽高及字体大小
+     * 设置二维码图片宽高和外边距及字体大小
      *
-     * @param float $width 图片宽度
-     * @param float $height 图片高度
+     * @param integer $widthHeight 图片宽度
      * @param integer $fontSize 字体大小
-     * @return void
+     * @return object
      */
-    public function setCodeImgeFontSize(float $width, float $height, int $fontSize)
+    public function setWidHeiMarFons(int $widthHeight, int $margin, int $fontSize)
     {
+        $this->imgWidthHeight = $widthHeight;
+        $this->imgMargin = $margin;
+        $this->fontSize = $fontSize;
+        return $this;
+    }
 
+    /**
+     * 设置二维码LOGO
+     *
+     * @param string $path LOGO路径
+     * @param integer $width LOGO宽度
+     * @param integer $height LOGO高度
+     * @return object
+     */
+    public function setLogo(string $path, int $width, int $height)
+    {
+        $this->logoPath = $path;
+        $this->logoWidth = $width;
+        $this->logoHeight = $height;
+        return $this;
+    }
+
+    /**
+     * 设置二维码颜色及背景色
+     *
+     * @param array $color [0, 0, 0, 0] rgba颜色，透明度0-1，0表不透明
+     * @param array $background [0, 0, 0, 0] rgba颜色，透明度0-1，0表不透明
+     * @return object
+     */
+    public function setColorBack(array $color, array $background)
+    {
+        if (!empty($color)) {
+            $this->qrcodeColor["r"] = $color[0];
+            $this->qrcodeColor["g"] = $color[1];
+            $this->qrcodeColor["b"] = $color[2];
+            $this->qrcodeColor["a"] = $color[3];
+        }
+        if (!empty($background)) {
+            $this->qrcodeBackground["r"] = $background[0];
+            $this->qrcodeBackground["g"] = $background[1];
+            $this->qrcodeBackground["b"] = $background[2];
+            $this->qrcodeBackground["a"] = $background[3];
+        }
+        return $this;
     }
 
     /**
@@ -186,11 +250,53 @@ class BatchQRCode
      * @param string $fileName 文件名，如：1，2，3，4·····
      * @param string $content 二维码内容，用于生成二维码
      * @param string $describe 二维码描述内容
-     * @return bool
+     * @return bool|string 返回文件地址或false
      */
-    public function createQRCodeImg(string $fileName, string $content, string $describe)
+    public function createQRCode(string $fileName, string $content, string $describe)
     {
+        try {
+            // 实例化二维码生成器，并传入内容
+            $qrCode = new QrCode($this->qrcodePrefix . $content . $this->qrcodeSuffix);
 
+            // 内容区域宽高,默认为300
+            $qrCode->setSize($this->imgWidthHeight);
+            // 外边距大小
+            $qrCode->setMargin($this->imgMargin);
+            // 设置编码
+            $qrCode->setEncoding('UTF-8');
+            // 设置容错等级
+            $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
+            // 设置二维码颜色,默认为黑色
+            $qrCode->setForegroundColor($this->qrcodeColor);
+            // 设置二维码背景色,默认为白色
+            $qrCode->setBackgroundColor($this->qrcodeBackground);
+
+            // 判断是否需要在二维码下方加描述
+            $describe = $this->describePrefix . $describe . $this->describeSuffix;
+            if (!empty($describe)) {
+                // 设置二维码下方的文字
+                $qrCode->setLabel($describe, $this->fontSize, null, LabelAlignment::CENTER());
+            }
+
+            // 判断是否需要给二维码加LOGO
+            if (!empty($this->logoPath)) {
+                // 设置LGO图片路径
+                $qrCode->setLogoPath($this->logoPath);
+                // 设置LOGO宽高
+                $qrCode->setLogoSize($this->logoWidth, $this->logoHeight);
+            }
+            
+            // 启用内置的验证读取器(默认情况下禁用)
+            $qrCode->setValidateResult(false);
+
+            // 二维码存在本地
+            $savePath = $this->savePath . $fileName . '.png';
+            $qrCode->writeFile($savePath);
+            // 返回文件保存地址
+            return $savePath;
+        } catch (\Throwable $th) {
+            return false;
+        }
     }
 
     /**
@@ -200,8 +306,53 @@ class BatchQRCode
      * @param integer $end 接收行数
      * @return bool
      */
-    public function getExcelCreateQRCode(int $start = 0, int $end = 0)
+    public function getExcel(int $start = 0, int $end = 0)
     {
-
+        // 限制开始行数最小为2
+        $start = max(2, $start);
+        //根据类型创建合适的读取器对象  这里写死了
+        $objread = IOFactory::createreader('Xlsx');
+        // 读取Excel文件
+        $objspreadsheet = $objread->load($this->excelPath);
+        // 获取第一张表
+        $objworksheet = $objspreadsheet->getsheet(0);
+        // 将数据转换为数组
+        $data = $objworksheet->toarray();
+        // 取得二维码内容所在列
+        $contCol = [];
+        foreach ($this->qrcodeArr as $name) {
+            foreach ($data[0] as $key => $val) {
+                if ($name == $val) {
+                    $contCol[] = $key;
+                }
+            }
+        }
+        // 取得描述内容所在列
+        $descrCol = [];
+        foreach ($this->describeArr as $name) {
+            foreach ($data[0] as $key => $val) {
+                if ($name == $val) {
+                    $descrCol[] = $key;
+                }
+            }
+        }
+        // 开始生成二维码
+        foreach ($data as $key => $val) {
+            if ($start == $end) {
+                return true;
+            } else {
+                $content = '';
+                foreach ($contCol as $col) {
+                    $content .= $val[$col];
+                }
+                $describe = '';
+                foreach ($descrCol as $col) {
+                    $describe .= $val[$col];
+                }
+                $this->createQRCode((string)$start, $content, $describe);
+                $end++;
+            }
+        }
+        return false;
     }
 }
---
+++ test/test.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * 
+ * 扩展库测试单元
+ * 
+ */
+
+// 引入自动加载
+require '../vendor/autoload.php';
+
+// 实例化二维码批量生成类
+// $batQR = new Bearke\BatchQRCode();
+
+// 实例化时可传入Excel文件路径
+$batQR = new Bearke\BatchQRCode('./static/excel/test.xlsx');
+
+
+/**
+ * #####类方法描述#####
+ * 
+ * setExcelPath('./static/excel/test.xlsx');                // 设置需要用于生成二维码的Excel文件路径
+ * 
+ * setQRCodeField(['name', 'age']);                         // 选择Excel表内的字段,用于生成二维码,支持多字段，拼接顺序与传入顺序有关
+ * 
+ * setQRCodePrefixSuffix('二维码内容前缀', '二维码内容后缀'); // 二维码内容的前缀与后缀，最后会与传入的Excel字段名对应的值拼接在一起
+ * 
+ * setDescrField(['msg1', 'msg2']);                         // 选择Excel表内的字段,用于描述二维码,支持多字段，拼接顺序与传入顺序有关
+ * 
+ * setDescrPrefixSuffix('描述内容前缀', '描述内容后缀');      // 二维码描述内容的前缀与后缀，最后会与传入的Excel字段名对应的值拼接在一起
+ * 
+ * setSavePath('./static/qrcode/');                         // 设置生成的二维码的保存路径,强烈建议调用它，会给你减少很多麻烦
+ * 
+ * setWidHeiMarFons(300, 10, 14);                           // 设置二维码宽高，外边距，底部描述内容字体大小
+ * 
+ * setLogo('./static/img/logo.png', 50, 50);                // 设置二维码中间的LOGO的路径，宽度，高度
+ * 
+ * setColorBack([0,0,0,0], [255,255,255,0]);                // 设置二维码的颜色与背景色，rgba颜色模式
+ * 
+ * createQRCode('001', "https://www.baidu.com", "百度一下"); // 生成单个二维码，文件名、二维码内容、二维码描述内容
+ * 
+ * getExcel(2, 0);                                           // 读取Excel表批量生成二维码,开始行数最小值为2，结束行数为0时表示不限制
+ * 
+ * 
+ * 重点：支持连贯操作->->->->,所有函数在getExcel()或CreateQRCode()之前调用即可，连贯操作时这两个只能二选一·········
+ */
+
+
+// 设置二维码保存路径(默认'./static/qrcode/img/')
+$batQR->setSavePath('./static/qrcode/img/')->setQRCodeField(["防伪码"])->setDescrField(["防伪验证码"])->getExcel(2, 0);
+
+// 生成单个二维码
+// $batQR->createQRCode('001', "https://www.baidu.com", "百度一下");
